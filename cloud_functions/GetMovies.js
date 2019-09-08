7d549e610ba63f443ff087b953e7b9cb95a51eeb
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-09-08 12:13:48 +0900

    get release dates

--- cloud_functions/GetMovies.js
+++ cloud_functions/GetMovies.js
@@ -22,24 +22,36 @@ exports.getMovies = async (req, res) => {
 };
 
 function getMovies() {
-  return [...document.querySelectorAll('.txt-box')].map(movie => {
-    const a = movie.querySelector('.title > a');
-    const title = a.textContent;
-    const href = a.href;
+  const base_section = document.querySelector('section');
+  const elements = base_section.children;
 
-    let cast_list = movie.querySelector('ul.cast-staff > li:nth-child(2)');
-    if (!cast_list) {
-      cast_list = movie.querySelector('ul.cast-staff > li:nth-child(1)');
+  let movies = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].tagName === 'H2') {
+      let release_date = elements[i].querySelector('span.icon.calendar').textContent;
+      this.release_date = release_date
+    } else if (elements[i].tagName === 'DIV') {
+      let txt_box = elements[i].querySelector('.txt-box');
+      let a = txt_box.querySelector('.title > a');
+      let title = a.textContent;
+      let href = a.href;
+
+      let cast_list = elements[i].querySelector('ul.cast-staff > li:nth-child(2)');
+      if (!cast_list) {
+        cast_list = elements[i].querySelector('ul.cast-staff > li:nth-child(1)');
+      }
+      let cast1 = cast_list.querySelector('span:nth-child(1)') ? cast_list.querySelector('span:nth-child(1)').textContent : '';
+      let cast2 = cast_list.querySelector('span:nth-child(2)') ? cast_list.querySelector('span:nth-child(2)').textContent : '';
+      let cast3 = cast_list.querySelector('span:nth-child(3)') ? cast_list.querySelector('span:nth-child(3)').textContent : '';
+      let cast4 = cast_list.querySelector('span:nth-child(4)') ? cast_list.querySelector('span:nth-child(4)').textContent : '';
+      let cast5 = cast_list.querySelector('span:nth-child(5)') ? cast_list.querySelector('span:nth-child(5)').textContent : '';
+      let casts = {cast1, cast2, cast3, cast4, cast5};
+
+      let description = elements[i].querySelector('.txt').textContent;
+      let movie = {release_date, title, href, casts, description};
+      movies.push(movie);
+      this.movies = movies
     }
-    const cast1 = cast_list.querySelector('span:nth-child(1)') ? cast_list.querySelector('span:nth-child(1)').textContent : '';
-    const cast2 = cast_list.querySelector('span:nth-child(2)') ? cast_list.querySelector('span:nth-child(2)').textContent : '';
-    const cast3 = cast_list.querySelector('span:nth-child(3)') ? cast_list.querySelector('span:nth-child(3)').textContent : '';
-    const cast4 = cast_list.querySelector('span:nth-child(4)') ? cast_list.querySelector('span:nth-child(4)').textContent : '';
-    const cast5 = cast_list.querySelector('span:nth-child(5)') ? cast_list.querySelector('span:nth-child(5)').textContent : '';
-    const casts = {cast1, cast2, cast3, cast4, cast5}
-
-    const description = movie.querySelector('.txt').textContent;
-
-    return {title, href, casts, description};
-  });
+  }
+  return movies;
 }
