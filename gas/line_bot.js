88379bcfcd640805f6c1169ae7841cb0b5a5b5b6
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-09-18 01:57:41 +0900

    refactor line-bot action

--- gas/line_bot.js
+++ gas/line_bot.js
@@ -1,40 +1,41 @@
 // LINE API
 var ACCESS_TOKEN = '<YOUR LINE ACCESS TOKEN>';
-var HEADERS = {"Content-Type": "application/json", 'Authorization': 'Bearer ' + ACCESS_TOKEN};
+var HEADERS      = {"Content-Type": "application/json", 'Authorization': 'Bearer ' + ACCESS_TOKEN};
+var PUSH_URL     = "https://api.line.me/v2/bot/message/push";
+
+// スプレッドシート
+var ss              = SpreadsheetApp.openById('<YOUR SPREAD SHEET ID>');
+var ws              = ss.getSheetByName('DB');
+var lastRow         = ws.getDataRange().getLastRow();
+var logSheet        = ss.getSheetByName('error_log');
+var logSheetLastRow = logSheet.getDataRange().getLastRow();
 
 // Gmail（エラー通知用）
-var TO = '<YOUR GMAIL ADDRESS>';
+var MY_GMAIL      = '<YOUR GMAIL ADDRESS>';
 var LOG_SHEET_URL = '<YOUR LOG SHEET URL>';
 
-function pushMessages() {
-  const URL = "https://api.line.me/v2/bot/message/push";
+// その他
+var now = new Date();
+var errorCount = 0;
 
-  var ss = SpreadsheetApp.openById('<YOUR SPREAD SHEET ID>');
-  var ws = ss.getSheetByName('DB');
-  var lastRow = ws.getDataRange().getLastRow();
-
-  var errorCount = 0;
-
-  var movies = getMovies(ws, lastRow);
+function pushNewArrival() {
+  const movies = getMovies(false);
 
   // 未通知の映画を全件送信する
   for (i = 0; i < movies.length; i++) {
     try {
-      var postData = getPostData(movies[i]);
-      var options = {
+      const postData = getPostData(movies[i], false);
+      const options = {
         "method": "post",
         "headers": HEADERS,
         "payload": JSON.stringify(postData)
       };
-      UrlFetchApp.fetch(URL, options);
+      UrlFetchApp.fetch(PUSH_URL, options);
       // LINE通知ステータスを1に切り替える
       ws.getRange(movies[i]['row'], 11).setValue(1);
     } catch(error) {
-      var logSheet = ss.getSheetByName('error_log');
-      var logSheetLastRow = logSheet.getDataRange().getLastRow();
-      var errorDate = new Date();
       errorCount++;
-      logSheet.getRange(logSheetLastRow + 1, 1).setValue(errorDate.toLocaleString().slice(0, 18));
+      logSheet.getRange(logSheetLastRow + 1, 1).setValue(now.toLocaleString().slice(0, 18));
       logSheet.getRange(logSheetLastRow + 1, 2).setValue(movies[i]['title']);
       logSheet.getRange(logSheetLastRow + 1, 3).setValue(movies[i]['url']);
       logSheet.getRange(logSheetLastRow + 1, 4).setValue(error);
@@ -44,46 +45,61 @@ function pushMessages() {
   }
 
   if (errorCount >= 1) {
-    MailApp.sendEmail(TO, '[ERROR] 新着映画情報bot', '下記URLからエラー内容を確認し、対応して下さい。' + '\n' + LOG_SHEET_URL);
+    MailApp.sendEmail(MY_GMAIL, '[ERROR] 新着映画情報bot', '下記URLからエラー内容を確認し、対応して下さい。' + '\n' + LOG_SHEET_URL);
   }
 }
 
-function getMovies(ws, lastRow) {
-  // 全件取得
-  var allMovies = ws.getRange(1, 1, lastRow, 11).getValues();
+function pushReminder() {
+  const movies = getMovies(true);
 
-  // 公開予定日が決まっている && LINE未通知の映画を配列に詰め込んでいく
+  // 未通知の映画を全件送信する
+  for (i = 0; i < movies.length; i++) {
+    try {
+      const postData = getPostData(movies[i], true);
+      const options = {
+        "method": "post",
+        "headers": HEADERS,
+        "payload": JSON.stringify(postData)
+      };
+      UrlFetchApp.fetch(PUSH_URL, options);
+    } catch(error) {
+      logSheet.getRange(logSheetLastRow + 1, 1).setValue(now.toLocaleString().slice(0, 18));
+      logSheet.getRange(logSheetLastRow + 1, 2).setValue(movies[i]['title']);
+      logSheet.getRange(logSheetLastRow + 1, 3).setValue(movies[i]['url']);
+      logSheet.getRange(logSheetLastRow + 1, 4).setValue(error);
+      // エラーの場合はLINE通知ステータスを2に切り替える
+      ws.getRange(movies[i]['row'], 11).setValue(2);
+    }
+  }
+
+  if (errorCount >= 1) {
+    MailApp.sendEmail(MY_GMAIL, '[ERROR] 新着映画情報bot', '下記URLからエラー内容を確認し、対応して下さい。' + '\n' + LOG_SHEET_URL);
+  }
+}
+
+function getMovies(reminderFlag) {
+  // 全件取得
+  const allMovies = ws.getRange(2, 1, lastRow, 11).getValues();
+
   var movies = [];
   for (i = 0; i < allMovies.length; i++) {
-    var row = i + 1; // 最後にLINE通知ステータスを切り替える際に使用
+    var releaseDate = allMovies[i][0],
+        pushStatus  = allMovies[i][10];
 
-    var releaseDate = allMovies[i][0];
-    var title       = allMovies[i][1];
-    var url         = allMovies[i][2];
-    var cast1       = allMovies[i][3];
-    var cast2       = allMovies[i][4];
-    var cast3       = allMovies[i][5];
-    var cast4       = allMovies[i][6];
-    var cast5       = allMovies[i][7];
-    var description = allMovies[i][8];
-    var imageUrl    = allMovies[i][9];
-    var pushStatus  = allMovies[i][10];
+    // 公開予定日が決まっていない場合は次の映画へ
+    var isReleaseDateFixed = Object.prototype.toString.call(releaseDate).slice(8, -1) === 'Date';
+    if (!isReleaseDateFixed) {
+      continue;
+    }
 
-    // 公開予定日が決まっている && LINE未通知なら連想配列に追加
-    if (Object.prototype.toString.call(releaseDate).slice(8, -1) === 'Date' && pushStatus === 0) {
-      var dict = {};
-      dict['row']         = row;
-      dict['releaseDate'] = releaseDate;
-      dict['title']       = title;
-      dict['url']         = url;
-      dict['cast1']       = cast1;
-      dict['cast2']       = cast2;
-      dict['cast3']       = cast3;
-      dict['cast4']       = cast4;
-      dict['cast5']       = cast5;
-      dict['description'] = description;
-      dict['imageUrl']    = imageUrl;
-      movies.push(dict);
+    if (reminderFlag) {
+      // 公開日の1週間前なら連想配列に追加
+      var sevenDaysBeforeRelease = getSevenDaysBeforeRelease(releaseDate).toLocaleString().slice(0, 10);
+      var today = now.toLocaleString().slice(0, 10);
+      movies = sevenDaysBeforeRelease === today ? getMovieDict(movies, allMovies[i], i, releaseDate) : movies;
+    } else {
+      // LINE未通知なら連想配列に追加
+      movies = pushStatus === 0 ? getMovieDict(movies, allMovies[i], i, releaseDate) : movies;
     }
   }
 
@@ -95,18 +111,41 @@ function getMovies(ws, lastRow) {
   }
 }
 
-function getPostData(movie) {
-  const USER_ID = 'YOUR USER ID';
+function getMovieDict(movies, movie, i, releaseDate) {
+  var dict = {};
+  dict['row']         = i + 2; // 最後にLINE通知ステータスを切り替える際に使用
+  dict['releaseDate'] = releaseDate;
+  dict['title']       = movie[1];
+  dict['url']         = movie[2];
+  dict['cast1']       = movie[3];
+  dict['cast2']       = movie[4];
+  dict['cast3']       = movie[5];
+  dict['cast4']       = movie[6];
+  dict['cast5']       = movie[7];
+  dict['description'] = movie[8];
+  dict['imageUrl']    = movie[9];
+  movies.push(dict);
+  return movies;
+}
 
-  var dateString = movie['releaseDate'].toLocaleString().slice(5, 10);
-  var title      = '【' + dateString + '公開】' + movie['title'];
-  var text       = [movie['cast1'], movie['cast2'], movie['cast3'], movie['cast4'], movie['cast5']].join(' / ') + ' / etc...';
+function getPostData(movie, reminderFlag) {
+  const USER_ID    = '<YOUR USER ID>';
+  const dateString = movie['releaseDate'].toLocaleString().slice(5, 10);
+  const title      = '【' + dateString + '公開】' + movie['title'];
+  const text       = [movie['cast1'], movie['cast2'], movie['cast3'], movie['cast4'], movie['cast5']].join(' / ') + ' / etc...';
 
-  var postData = {
+  var altText;
+  if (reminderFlag) {
+    altText = "公開直前の映画情報だよ！";
+  } else {
+    altText = "来月公開の映画情報だよ！";
+  }
+
+  const postData = {
     "to": USER_ID,
     "messages": [{
       "type": "template",
-      "altText": "新着映画情報だよ！",
+      "altText": altText,
       "template": {
         "type": "carousel",
         "columns": [{
@@ -122,7 +161,7 @@ function getPostData(movie) {
             },
             {
               "type":"postback",
-              "label":"カレンダーに登録する",
+              "label": "カレンダーに登録する",
               "data": 'date=' + movie['releaseDate'].toLocaleString().slice(0, 10) + '&' + 'title=' + movie['title'],
               "displayText": "カレンダーに登録する",
             },
@@ -134,10 +173,15 @@ function getPostData(movie) {
   return postData;
 }
 
+function getSevenDaysBeforeRelease(date) {
+  date.setDate(date.getDate() - 7);
+  return date;
+}
+
 // replyに対するアクション
 function doPost(e) {
-  var json = JSON.parse(e.postData.contents);
-  var events = json["events"];
+  const json = JSON.parse(e.postData.contents);
+  const events = json["events"];
 
   for(var i = 0; i < events.length; i++){
     if(events[i].type == "message"){
@@ -150,24 +194,24 @@ function doPost(e) {
 }
 
 function createEvents(e) {
-  var calender = CalendarApp.getCalendarById('<YOUR CALENDER ID>');
+  const calender = CalendarApp.getCalendarById(MY_GMAIL);
   // dataは"date=2019/01/01&title=movie_title"という形式で渡されるため、5~14文字目を切り出せばに公開日が、22文字目以降を切り出せば映画タイトルが取得できる
-  var data = e.postback.data;
-  var date = new Date(data.slice(5, 15));
-  var title = data.substr(22);
+  const data = e.postback.data;
+  const date = new Date(data.slice(5, 15));
+  const title = data.substr(22);
   calender.createAllDayEvent(title, date);
 }
 
 function replyMessage(e) {
   const REPLY_URL = "https://api.line.me/v2/bot/message/reply";
-  var postData = {
+  const postData = {
     "replyToken": e.replyToken,
     "messages": [{
         "type": "text",
         "text": e.type == "message" ? e.source.userId : "カレンダーに登録しました！",
       }]
   };
-  var options = {
+  const options = {
     "method": "post",
     "headers": HEADERS,
     "payload": JSON.stringify(postData),
