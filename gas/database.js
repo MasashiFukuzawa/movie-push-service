9d06a6b034a5b50f44da3c7b2931531aef48c8ad
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-09-21 15:40:54 +0900

    clean unnecessary datas regularly

--- gas/database.js
+++ gas/database.js
@@ -1,60 +1,67 @@
 function cleanSpreadSheet() {
   const ss = SpreadsheetApp.openById('<YOUR SPREAD SHEET ID>');
   const ws = ss.getSheetByName('DB');
-  const lastRow = ws.getDataRange().getLastRow();
-  const nextYearMonth = getNextYearMonth();
+  const lastRow = ws.getLastRow();
+  const lastCol = ws.getLastColumn();
 
-  var nextMonthMovies = [];
-  for(var i　=　2;　i　<=　lastRow ;　i++){
-    var date = ws.getRange(i, 1).getValue();
-    var dateString = date.toLocaleString().slice(0, 10);
+  var allMovies = ws.getRange(2, 1, lastRow - 1, lastCol).getValues();
+  var movieNum  = allMovies.length;
 
-    deleteReleaseDateNotFixed(ws, i, date);
+  const movieTitles = allMovies.map(function(movie) {
+    return movie[1]
+  });
 
-    var title = getMovieTitle(ws, i, nextYearMonth, dateString);
+  for(var i　=　0;　i　<　movieNum;　i++){
+    var date  = allMovies[i][0];
+    var title = allMovies[i][1];
 
-    if (title) {
-      deleteDuplicateDatas(ws, i, nextMonthMovies, title);
-      nextMonthMovies.push(title);
+    // allMoviesのうち、A列がDate型でないものを配列から削除
+    if (releaseDateNotFixed(date)) {
+      allMovies.splice(i, 1);
+      movieNum--;
+      i--;
+      continue;
+    }
+
+    // allMoviesの配列中に同titleがあれば削除
+    if (duplicateDataPresent(allMovies, movieTitles, title, i)) {
+      var a = allMovies.splice(i, 1);
+      //Logger.log([a, i, movieNum].join(' / '));
+      movieNum--;
+      i--;
     }
   }
+
+  const rows  = allMovies.length;
+  const cols  = allMovies[0].length;
+  const range = ws.getRange(2, 1, rows, cols);
+
+  // 一度シートを全消しした後、必要なデータだけを再度セットしていく
+  ws.clearContents();
+  ws.getRange(1, 1, 1, cols).setValues([['公開予定日', 'タイトル', 'URL', 'キャスト', '', '', '', '', 'あらすじ', '画像URL', 'LINE通知']]);
+  range.setValues(allMovies);
 }
 
-function getNextYearMonth() {
-  const now = new Date();
-  const year  = now.getFullYear(),
-        month = now.getMonth();
-
-  var nextYearMonth; // nextYearMonthは"2019/01"のようなString型
-  if (month === 12) {
-    nextYearMonth = (year + 1) + '/01';
-  } else {
-    nextYearMonth = year + '/' + ("0" + (month + 2)).slice(-2);
-  }
-  return nextYearMonth;
-}
-
-function deleteReleaseDateNotFixed(ws, i, date) {
-  // A列がDate型でないものを削除
+function releaseDateNotFixed(date) {
   if(Object.prototype.toString.call(date).slice(8, -1) !== 'Date'){
-    ws.deleteRows(i);
+    return true;
   }
+  return false
 }
 
-function getMovieTitle(ws, i, nextYearMonth, dateString) {
-  var regexp = new RegExp('^' + nextYearMonth);
-  // 来月公開の映画であればタイトルを取得
-  if (regexp.test(dateString)) {
-    var title = ws.getRange(i, 2).getValue();
-    return title;
-  } else {
-    return false;
+function duplicateDataPresent(movies, titles, title, i) {
+  const index = titles.indexOf(title);
+  // 同一映画データを古い方から削除することで、imageUrlが取得できていなかったものや、公開日が未定だったものについてアップデートが可能となる
+  if (index !== i) {
+    const oldLinePushStatus = movies[i][10];
+    if (oldLinePushStatus === 1) {
+      // 古いデータ削除する前に、古いデータのLINE通知フラグを新しい方の映画データに引き継がせておく必要がある
+      // movies[index][10]はnewLinePushStatusを意味している
+      Logger.log(movies[i]);
+      Logger.log(movies[index]);
+      movies[index][10] = 1;
+    }
+    return true;
   }
-}
-
-function deleteDuplicateDatas(ws, i, nextMonthMovies, title) {
-  // nextMonthMoviesの配列中に同titleがあれば削除
-  if (nextMonthMovies.indexOf(title) !== -1) {
-    ws.deleteRows(i);
-  }
+  return false;
 }
